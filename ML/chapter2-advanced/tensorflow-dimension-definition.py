057c053091bdbbb01ee12cfe408f31fddc113a0f
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2026-07-14 16:49:48 +0900

    Start course 2 - advanced ml

---
+++ ML/chapter2-advanced/tensorflow-dimension-definition.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# 기존 넘파이에서 정의하던 방식 
+a = np.array([200,17]) # 1-D.  행/열 개념이 없는 1차원 배열임 
+print(a)
+print(a.shape) # (2, ) -> 요소 2개를 가지는 1차원 배열 
+
+b = a.reshape(-1,1)
+print(b)
+print(b.shape)
+
+
+
+# Tensorflow에서 정의하는 방식 
+
+x = np.array([[200,17]]) # [[]]을 두번 사용함! . 1 * 2 행벡터 
+print(x)
+print(x.shape)
+
+x = np.array([[200], [17]]) # 2 * 1 열벡터 
+print(x)
+print(x.shape)

9d30567f345e3fb26d8eb147792b02f731e75ded
kangmin04 <146157505+kangmin04@users.noreply.github.com>
2026-08-09 22:56:48 +0900

    Feat. learn activation function

--- ML/chapter2-advanced/week1/lab01-nuerons.py
+++ ML/chapter2-advanced/week1/lab01-nuerons.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
+from tf.keras.layers import Dense, Input
 from tensorflow.keras import Sequential
 from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
 from tensorflow.keras.activations import sigmoid
-from lab_utils_common import dlc
-from lab_neurons_utils import plt_prob_1d, sigmoidnp, plt_linear, plt_logistic
+# from lab_utils_common import dlc
+# from lab_neurons_utils import plt_prob_1d, sigmoidnp, plt_linear, plt_logistic
 # plt.style.use('./deeplearning.mplstyle')
 import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
@@ -22,3 +22,46 @@ ax.legend( fontsize='xx-large')
 ax.set_ylabel('Price (in 1000s of dollars)', fontsize='xx-large')
 ax.set_xlabel('Size (1000 sqft)', fontsize='xx-large')
 plt.show()
+
+
+linear_layer = tf.keras.layers.Dense(units=1, activation = 'linear', )
+linear_layer.get_weights()
+
+a1 = linear_layer(X_train[0].reshape(1,1))
+print(a1)
+
+w, b= linear_layer.get_weights()
+print(f"w = {w}, b={b}")
+
+set_w = np.array([[200]])
+set_b = np.array([100])
+
+# set_weights takes a list of numpy arrays
+linear_layer.set_weights([set_w, set_b])
+print(linear_layer.get_weights())
+
+a1 = linear_layer(X_train[0].reshape(1,1))
+print(a1)
+alin = np.dot(set_w,X_train[0].reshape(1,1)) + set_b
+print(alin)
+
+prediction_tf = linear_layer(X_train)
+prediction_np = np.dot( X_train, set_w) + set_b
+
+# plt_linear(X_train, Y_train, prediction_tf, prediction_np)
+
+
+
+# logistic regerssion
+model = Sequential(
+    [
+        tf.keras.layers.Dense(1, input_dim=1,  activation = 'sigmoid', name='L1')
+    ]
+)
+
+model.summary() # shows the layers and number of parameters in the mode
+
+logistic_layer = model.get_layer('L1')
+w,b = logistic_layer.get_weights()
+print(w,b)
+print(w.shape,b.shape)
